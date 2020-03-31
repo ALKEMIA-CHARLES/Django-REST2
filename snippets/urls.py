0fa1f7f437eb1cd3b9777f330fe463caf2cc9d6d
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-03-31 22:48:23 +0300

    added confs

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,29 @@
 from django.conf.urls import url
 from . import views
+from rest_framework import renderers
 from rest_framework.urlpatterns import format_suffix_patterns
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.routers import DefaultRouter
+
+# snippet_list = SnippetViewSet.as_view({
+#     'get': 'list',
+#     'post': 'create'
+# })
+# snippet_detail = SnippetViewSet.as_view({
+#     'get': 'retrieve',
+#     'put': 'update',
+#     'patch': 'partial_update',
+#     'delete': 'destroy'
+# })
+# snippet_highlight = SnippetViewSet.as_view({
+#     'get': 'highlight'
+# }, renderer_classes=[renderers.StaticHTMLRenderer])
+# user_list = UserViewSet.as_view({
+#     'get': 'list'
+# })
+# user_detail = UserViewSet.as_view({
+#     'get': 'retrieve'
+# })
 
 urlpatterns = format_suffix_patterns([
     url(r'^$', views.api_root),
@@ -11,4 +33,5 @@ urlpatterns = format_suffix_patterns([
     url(r'^users/(?P<pk>\d+)/$', views.UserDetail.as_view(), name='user-detail'),
     url(r'^snippets/(?P<pk>\d+)/highlight/$', views.SnippetHighlight.as_view(), name='snippet-highlight')
 ])
+
 
