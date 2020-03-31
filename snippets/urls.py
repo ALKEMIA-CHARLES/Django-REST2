b701f944e2ba631f259389386123bed225eff909
ALKEMIA-CHARLES <charlesmtawaliJr@gmail.com>
2020-03-31 12:11:57 +0300

    added CBVS

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from . import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns =[
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>\d+)/$', views.snippet_detail)
+    url(r'^snippets/$', views.snippet_list.as_view()),
+    url(r'^snippets/(?P<pk>\d+)/$', views.snippet_detail.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
